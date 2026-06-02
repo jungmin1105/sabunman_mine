077ee13085dcaf4c641f3a959af5583c1f8f9856
jungmin1105 <jungmin08.ha@sasa.hs.kr>
2026-06-02 10:30:11 +0900

    Add route for editing a post

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Auth from "./pages/Auth";
 import Feed from "./pages/Feed";
 import NewPost from "./pages/NewPost";
 import PostDetail from "./pages/PostDetail";
+import EditPost from "./pages/EditPost";
 import MapPage from "./pages/Map";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
@@ -40,6 +41,7 @@ const App = () => (
             <Route path="/feed" element={<ProtectedRoute><Feed /></ProtectedRoute>} />
             <Route path="/new" element={<ProtectedRoute><NewPost /></ProtectedRoute>} />
             <Route path="/post/:id" element={<ProtectedRoute><PostDetail /></ProtectedRoute>} />
+            <Route path="/post/:id/edit" element={<ProtectedRoute><EditPost /></ProtectedRoute>} />
             <Route path="/map" element={<ProtectedRoute><MapPage /></ProtectedRoute>} />
             <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
             <Route path="*" element={<NotFound />} />
